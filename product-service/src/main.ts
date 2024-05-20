d0b0e2180730040b11605392d5d001190d6b547b
Pipo <156736428+pannathatdev@users.noreply.github.com>
2024-05-20 10:29:05 +0700

    แก้ไข port

--- product-service/src/main.ts
+++ product-service/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000); // เปลี่ยนพอร์ตจาก 3000 เป็น 8000
+  await app.listen(9000);
 }
 bootstrap();

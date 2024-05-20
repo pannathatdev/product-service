de160c0b908f1aa0a954bd64b9f6b3cbf1f960d1
Pipo <156736428+pannathatdev@users.noreply.github.com>
2024-05-20 12:42:07 +0700

    edit file test

--- product-service/src/app.controller.spec.ts
+++ product-service/src/app.controller.spec.ts
@@ -1,20 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppModule } from './app.module';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
       controllers: [AppController],
-      providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
-  describe('getHello', () => {
+  describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });

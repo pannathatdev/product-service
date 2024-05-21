7471b9c0383712b0af59eb06d279136615cd028c
Pipo <156736428+pannathatdev@users.noreply.github.com>
2024-05-21 16:24:40 +0700

    Update app.controller.spec.ts

--- product-service/src/app.controller.spec.ts
+++ product-service/src/app.controller.spec.ts
@@ -3,21 +3,19 @@ import { AppController } from './app.controller';
 import { AppModule } from './app.module';
 
 describe('AppController', () => {
-  let appController: AppController;
+    let appController: AppController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-      controllers: [AppController],
-    }).compile();
+    beforeEach(async () => {
+      const app: TestingModule = await Test.createTestingModule({
+        imports: [AppModule],
+        controllers: [AppController],
+      }).compile();
+  
+      appController = app.get<AppController>(AppController);
+    });
 
-    appController = app.get<AppController>(AppController);
+    it('root should return "Hello World!"', () => {
+      const result = appController.getHello();
+      expect(result).toBe('Hello World!');
+    });
   });
-
-  it('root should return "Hello World!"', () => {
-    expect(appController.getHello()).toBe('Hello World!');
-  });
-});
-
-// เพิ่ม timeout ที่ 10 วินาที (10000 milliseconds) ให้กับทุกการทดสอบในไฟล์นี้
-jest.setTimeout(10000);

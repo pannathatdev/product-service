0b8973bb2669a0f168d7364082b62c4062ecbfa0
Pipo <156736428+pannathatdev@users.noreply.github.com>
2024-05-24 15:21:18 +0700

    Update mockup-loader.js

--- product-service/src/product/mockup-loader.js
+++ product-service/src/product/mockup-loader.js
@@ -2,11 +2,11 @@ const axios = require('axios');
 
 async function loadMockupData() {
     try {
-        const productsUrl = 'https://raw.githubusercontent.com/pannathatdev/product-service/main/product-service/src/product/mock-products.json';
+        const productsUrl = 'https://raw.githubusercontent.com/pannathatdev/product-service/main/src/mock-products.json';
         const response = await axios.get(productsUrl);
         const products = response.data;
 
-        await axios.post('http://product-service:8000/orders/load', products); // เปลี่ยนจาก order-service เป็น product-service
+        await axios.post('http://order-service:9000/orders/load', products); // เปลี่ยนจาก product-service เป็น order-service
         console.log('Mockup data loaded successfully.');
     } catch (error) {
         console.error('Failed to load mockup data:', error.message);
